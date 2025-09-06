edcfa0615f4190aa39d02278be24bd12ef26b6cc
Edgars Eduards Hoveckis <eehoveckis@protonmail.com>
2025-09-06 17:06:57 +0300

    1.0.3 - All deco blocks

--- index.py
+++ index.py
@@ -14,9 +14,28 @@ keepProps = {
     "minecraft:cave_vines",
     "minecraft:twisting_vines",
     "minecraft:small_dripleaf",
-    "minecraft:barrier",
     "minecraft:oak_leaves",
-    "minecraft:azalea_leaves"
+    "minecraft:dark_oak_leaves",
+    "minecraft:azalea_leaves",
+    "minecraft:birch_sapling",
+    "minecraft:oak_sapling",
+    "minecraft:spruce_sapling",
+    "minecraft:jungle_sapling",
+    "minecraft:acacia_sapling",
+    "minecraft:dark_oak_sapling"
+}
+
+# Some custom blocks have 2 halves (upper/lower). Counting both of them will return wrong count.
+# Some 2-high blocks have tripwire (all values false) as base, that also can be ignored.
+ignoreKeys = {
+    "small_dripleaf[facing=north,half=upper,waterlogged=false]",
+    "small_dripleaf[facing=east,half=upper,waterlogged=false]",
+    "small_dripleaf[facing=west,half=upper,waterlogged=false]",
+    "tripwire[attached=false,disarmed=false,east=false,north=false,powered=false,south=false,west=false]",
+    "chorus_plant",
+    "light",
+    "air",
+    "barrier"
 }
 
 # Replace shcematic with yours.
@@ -35,10 +54,13 @@ for region in schem.regions.values():
         if key.startswith("minecraft:"):
             key = key[len("minecraft:"):]
 
+        if key in ignoreKeys:
+            continue
+
         displayName = customNames.get(key, key)
         blockCounter[displayName] += 1
 
-# Vanilla blocks that are two high, they need to be divided by two, otherwise it returns wrong count.
+# Vanilla blocks that have upper/lower hald, they need to be divided by two, otherwise it returns 2x block amount.
 doubleBlocks = {
     "lilac",
     "rose_bush",
@@ -52,7 +74,10 @@ doubleBlocks = {
     "acacia_door",
     "mangrove_door",
     "cherry_door",
-    "bamboo_door"
+    "bamboo_door",
+    "tall_grass",
+    "large_fern",
+    "pitcher_plant"
 }
 
 for key in list(blockCounter.keys()):
