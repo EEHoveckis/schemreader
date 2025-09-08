8a8c32226b942a042117dbbc191bffce3fe6f413
Edgars Eduards Hoveckis <eehoveckis@protonmail.com>
2025-09-08 22:53:58 +0300

    1.0.6 - Changes to wall blocks + polish

--- index.py
+++ index.py
@@ -2,47 +2,27 @@ import json
 from litemapy import Schematic
 from collections import Counter
 
+# Configuration file
+with open("config.json", "r", encoding="utf-8") as f:
+    config = json.load(f)
+
 # You can replace originrealms.json with your own blocks list.
 with open("originrealms.json", "r", encoding="utf-8") as f:
     customNames = json.load(f)
 
 # Blocks used in Origin Realms to make custom blocks and entities.
-keepProps = {
-    "minecraft:note_block",
-    "minecraft:tripwire",
-    "minecraft:sugar_cane",
-    "minecraft:cave_vines",
-    "minecraft:twisting_vines",
-    "minecraft:small_dripleaf",
-    "minecraft:oak_leaves",
-    "minecraft:dark_oak_leaves",
-    "minecraft:azalea_leaves",
-    "minecraft:birch_sapling",
-    "minecraft:oak_sapling",
-    "minecraft:spruce_sapling",
-    "minecraft:jungle_sapling",
-    "minecraft:acacia_sapling",
-    "minecraft:dark_oak_sapling"
-}
+keepProps = set(config.get("keepProps", []))
 
 # Some custom blocks have 2 halves (upper/lower). Counting both of them will return wrong count.
 # Some 2-high blocks have tripwire (all values false) as base, that also can be ignored.
-ignoreKeys = {
-    "small_dripleaf[facing=north,half=upper,waterlogged=false]",
-    "small_dripleaf[facing=east,half=upper,waterlogged=false]",
-    "small_dripleaf[facing=west,half=upper,waterlogged=false]",
-    "tripwire[attached=false,disarmed=false,east=false,north=false,powered=false,south=false,west=false]",
-    "chorus_plant",
-    "light",
-    "air",
-    "barrier"
-}
+ignoreKeys = set(config.get("ignoreKeys", []))
 
-# Replace shcematic with yours.
+# Vanilla blocks that have upper/lower half, they need to be divided by two, otherwise it returns 2x block amount.
+doubleBlocks = set(config.get("doubleBlocks", []))
+
 schem = Schematic.load("./schematics/schem.litematic")
 
 blockCounter = Counter()
-
 for region in schem.regions.values():
     for x, y, z in region.block_positions():
         block = region[x, y, z]
@@ -59,26 +39,6 @@ for region in schem.regions.values():
 
         displayName = customNames.get(key, key)
         blockCounter[displayName] += 1
-
-# Vanilla blocks that have upper/lower hald, they need to be divided by two, otherwise it returns 2x block amount.
-doubleBlocks = {
-    "lilac",
-    "rose_bush",
-    "sunflower",
-    "peony",
-    "spruce_door",
-    "oak_door",
-    "birch_door",
-    "jungle_door",
-    "dark_oak_door",
-    "acacia_door",
-    "mangrove_door",
-    "cherry_door",
-    "bamboo_door",
-    "tall_grass",
-    "large_fern",
-    "pitcher_plant"
-}
 
 for key in list(blockCounter.keys()):
     baseName = key.split("[")[0]
